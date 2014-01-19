c0720e4bdcce445e86621a344ad5354f5d8147f9
MainMemory <sonicmike2@yahoo.com>
2014-01-19 10:28:57 -0600

    Initial Commit

---
+++ BlupiEdit/LevelSelectForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BlupiEdit
+{
+    public partial class LevelSelectForm : Form
+    {
+        public LevelSelectForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlupiEdit/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BlupiEdit
+{
+    public partial class MainForm : Form
+    {
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (Program.Arguments.Length > 0)
+                LoadGame(Program.Arguments[0]);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fd = new OpenFileDialog() { DefaultExt = "exe", Filter = "EXE Files|*.exe", RestoreDirectory = true })
+                if (fd.ShowDialog(this) == DialogResult.OK)
+                    LoadGame(fd.FileName);
+        }
+
+        private void LoadGame(string filename)
+        {
+            LevelData.LoadGame(filename);
+            changeLevelToolStripMenuItem.Enabled = true;
+        }
+
+        private void changeLevelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (LevelSelectForm ls = new LevelSelectForm())
+                ls.ShowDialog(this);
+        }
+    }
+}
